23faeb85d34c451df49e0be5087a61a04430915a
Alain Achkar <alain.achkar@triacta.com>
2018-01-29 17:02:17 -0500

    Fix second error

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -13,4 +13,4 @@ class TestHello(unittest.TestCase):
 
     def test_world_unicode(self):
         """sample test with unicode"""
-        self.assertEqual(hello(u'world'), u'hello world')
+        self.assertEqual(hello(u'world'), u'Marhaba world')
